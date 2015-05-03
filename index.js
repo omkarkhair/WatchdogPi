ddddc8190ff10fd236f38bc27d5fa63c2a53e676
Omkar Khair <omkarkhair@gmail.com>
2015-05-03 08:11:59 +0000

    Changed pin to 16

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var gpio = require("pi-gpio");
 
 // Values we plan to move to config in coming versions
-var pin = 33;
+var pin = 16;
 var checkState = 0; // The state for you which you need a notifications. You can either be notified if the pin is open for too long, or closed.
 var maxTimeout = 30000; // maximum time for which the state can remain locked in milliseconds.
 var pollTime = 3000; // How often should we check the state, in milliseconds.
