d0f34e9880a1e315bf863b1aaa726d59edd2c34e
Omkar Khair <omkarkhair@gmail.com>
2015-05-07 01:18:16 +0530

    some stuff to get Mandrill to send emails

--- index.js
+++ index.js
@@ -1,12 +1,66 @@
 var gpio = require("pi-gpio");
+var mandrill = require('mandrill-api/mandrill');
+var config = require('config');
+
+// Setup mandrill
+var mandrill_key = config.get('mandrill-key');
+var mandrill_client = new mandrill.Mandrill(mandrill_key);
 
 // Values we plan to move to config in coming versions
-var pin = 29; // Set a GPIO Pin
-var checkState = 0; // The state for you which you need a notifications. You can either be notified if the pin is open for too long, or closed.
-var maxTimeout = 30000; // maximum time for which the state can remain locked in milliseconds.
-var pollTime = 3000; // How often should we check the state, in milliseconds.
+var pin = config.get('pin'); // Set a GPIO Pin
+var checkState = config.get('alertValue'); // The state for you which you need a notifications. You can either be notified if the pin is open for too long, or closed.
+var maxTimeout = config.get('maxTimeOut'); // maximum time for which the state can remain locked in milliseconds.
+var pollTime = config.get('pollTime'); // How often should we check the state, in milliseconds.
 
+// Mandrill alert message
+var message = {
+    "html": "<p>Bow wow! Watchdog found the door open.</p>",
+    "text": "Bow wow! Watchdog found the door open.",
+    "subject": "Watchdog Pi Alert!",
+    "from_email": "username@gmail.com",
+    "from_name": "Watchdog Pi",
+    "to": [{
+            "email": "guardian@example.com",
+            "name": "Guardian",
+            "type": "to"
+        }],
+    "headers": {
+        "Reply-To": "username@gmail.com"
+    },
+    "important": false,
+    "track_opens": null,
+    "track_clicks": null,
+    "auto_text": null,
+    "auto_html": null,
+    "inline_css": null,
+    "url_strip_qs": null,
+    "preserve_recipients": null,
+    "view_content_link": null,
+    "bcc_address": "message.bcc_address@example.com",
+    "tracking_domain": null,
+    "signing_domain": null,
+    "return_path_domain": null,
+    "merge": true,
+    "merge_language": "mailchimp",
+    
+};
+var async = false;
+var ip_pool = "Main Pool";
+var send_at = "example send_at";
+    
 var timer = null;
+
+// Alert routine
+function sendAlert() {
+    mandrill_client.messages.send({"message": message, "async": async, "ip_pool": ip_pool, "send_at": send_at}, function(result) {
+        console.log(result);
+        
+    }, function(e) {
+        
+        console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+        
+    });
+}
 
 var Status = {
     status : !checkState,
